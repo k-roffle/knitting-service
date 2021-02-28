5730c1f77146867dc2a4e972933cc3c669eb95f7
yool <yool@spoqa.com>
2021-02-28 19:54:23 +0900

    Feat: implement string toSlug extension function
    - See official tutorial, https://spring.io/guides/tutorials/spring-boot-kotlin/

---
+++ src/main/kotlin/com/kroffle/knitting/Extensions.kt
@@ -0,0 +1,8 @@
+package com.kroffle.knitting
+
+fun String.toSlug() = toLowerCase()
+        .replace("\n", " ")
+        .replace("[^a-z\\d\\s]".toRegex(), " ")
+        .split(" ")
+        .joinToString("-")
+        .replace("-+".toRegex(), "-")
