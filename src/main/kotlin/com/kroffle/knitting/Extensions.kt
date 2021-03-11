75871bd2d866c5f3475541a8126e9a799a82f73c
Yuri Han <yool@spoqa.com>
2021-03-11 23:32:19 +0900

    Revert "Initialize Kotlin + Spring project"

--- src/main/kotlin/com/kroffle/knitting/Extensions.kt
+++
@@ -1,8 +0,0 @@
-package com.kroffle.knitting
-
-fun String.toSlug() = toLowerCase()
-        .replace("\n", " ")
-        .replace("[^a-z\\d\\s]".toRegex(), " ")
-        .split(" ")
-        .joinToString("-")
-        .replace("-+".toRegex(), "-")
