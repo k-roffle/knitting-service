2ab62c1f75d3a112b37abedfc23a33e272524ad8
yool <yool@spoqa.com>
2021-03-14 13:08:10 +0900

    feat: add ping api router and handler

---
+++ src/main/kotlin/com/kroffle/knitting/handler/PingHandler.kt
@@ -0,0 +1,15 @@
+package com.kroffle.knitting.handler
+
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.ServerResponse.ok
+import reactor.core.publisher.Mono
+
+@Component
+class PingHandler {
+    fun get(req: ServerRequest): Mono<ServerResponse> = ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue("pong")
+}
