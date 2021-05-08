5cc01c29c8ba2e6b988e50eb2ce109f1152d80ca
Yuri Han <devuri404@gmail.com>
2021-05-08 18:00:51 +0900

    feat: add require condition of Length

--- src/main/kotlin/com/kroffle/knitting/domain/design/value/Length.kt
+++ src/main/kotlin/com/kroffle/knitting/domain/design/value/Length.kt
@@ -2,4 +2,8 @@ package com.kroffle.knitting.domain.design.value
 
 import com.kroffle.knitting.domain.design.enum.SizeUnitType
 
-class Length(val value: Double, val unit: SizeUnitType = SizeUnitType.Cm)
+class Length(val value: Double, val unit: SizeUnitType = SizeUnitType.Cm) {
+    init {
+        require(value > 0)
+    }
+}
