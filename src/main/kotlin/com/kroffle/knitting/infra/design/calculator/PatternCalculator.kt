b77feecdc612c4113b0f3d47a3d842edad778d40
Yuri Han <devuri404@gmail.com>
2021-05-08 23:22:24 +0900

    refactor: move marshalling function

--- src/main/kotlin/com/kroffle/knitting/infra/design/calculator/PatternCalculator.kt
+++ src/main/kotlin/com/kroffle/knitting/infra/design/calculator/PatternCalculator.kt
@@ -4,21 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.kroffle.knitting.domain.design.value.Gauge
 import com.kroffle.knitting.domain.design.value.Pattern
-import com.kroffle.knitting.infra.design.calculator.data.DraftPattern
+import com.kroffle.knitting.infra.design.calculator.data.toDraftPattern
 
 class PatternCalculator {
     private val mapper: ObjectMapper = ObjectMapper().registerKotlinModule()
 
-    private fun deserializePattern(pattern: Pattern): DraftPattern {
-        return mapper.readValue(pattern.value, DraftPattern::class.java)
-    }
-
-    private fun serializePattern(pattern: DraftPattern): Pattern {
-        return Pattern(mapper.writeValueAsString(pattern))
-    }
-
     fun calculateGauge(target: Pattern, origin: Gauge, my: Gauge): Pattern {
-        val deserializedTarget = deserializePattern(target)
-        return serializePattern(deserializedTarget.calculateBlocks(origin, my))
+        val draftPattern = target.toDraftPattern(mapper)
+        return draftPattern.calculateBlocks(origin, my).toPattern(mapper)
     }
 }
