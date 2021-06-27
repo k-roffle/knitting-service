23cd49a8f1052edbcc82fdbffbf4f76b77622c32
Yuri Han <devuri404@gmail.com>
2021-06-27 14:43:52 +0900

    fix: make knitter.email unique

--- src/main/resources/db/migration/V5__add_knitter_table.sql
+++ src/main/resources/db/migration/V5__add_knitter_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE knitter (
     id UUID NOT NULL,
-    email VARCHAR NOT NULL,
+    email VARCHAR NOT NULL UNIQUE,
     name VARCHAR,
     image_url VARCHAR,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL,
