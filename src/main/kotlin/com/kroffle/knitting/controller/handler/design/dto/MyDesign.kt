734fffc0c63e2d8bbe9e3ee09da0cde3ee466a81
Yuri Han <devuri404@gmail.com>
2021-08-01 16:05:55 +0900

    fix: make json key snake_case

--- src/main/kotlin/com/kroffle/knitting/controller/handler/design/dto/MyDesign.kt
+++ src/main/kotlin/com/kroffle/knitting/controller/handler/design/dto/MyDesign.kt
@@ -1,9 +1,12 @@
 package com.kroffle.knitting.controller.handler.design.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class MyDesign(
     val id: Long,
     val name: String,
     val yarn: String,
+    @JsonProperty("thumbnail_image_url")
     val thumbnailImageUrl: String?,
     val tags: List<String>,
 )
