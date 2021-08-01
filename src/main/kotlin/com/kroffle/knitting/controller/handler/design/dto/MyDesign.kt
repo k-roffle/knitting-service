a5cde9c98aaae2a54f5b54d366369bef4cd44448
Yuri Han <devuri404@gmail.com>
2021-08-01 14:46:43 +0900

    feat: add thumbnailImageUrl to response of myDeesign api

--- src/main/kotlin/com/kroffle/knitting/controller/handler/design/dto/MyDesign.kt
+++ src/main/kotlin/com/kroffle/knitting/controller/handler/design/dto/MyDesign.kt
@@ -3,5 +3,6 @@ package com.kroffle.knitting.controller.handler.design.dto
 data class MyDesign(
     val name: String,
     val yarn: String,
+    val thumbnailImageUrl: String?,
     val tags: List<String>,
 )
