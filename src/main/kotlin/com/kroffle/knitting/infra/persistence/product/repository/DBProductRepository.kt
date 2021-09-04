b35352a882e3f282a047bc423b15969b7044d96d
Yuri Han <devuri404@gmail.com>
2021-09-04 16:29:47 +0900

    feat: implement myProducts repository

--- src/main/kotlin/com/kroffle/knitting/infra/persistence/product/repository/DBProductRepository.kt
+++ src/main/kotlin/com/kroffle/knitting/infra/persistence/product/repository/DBProductRepository.kt
@@ -2,9 +2,12 @@ package com.kroffle.knitting.infra.persistence.product.repository
 
 import com.kroffle.knitting.domain.product.enum.InputStatus
 import com.kroffle.knitting.infra.persistence.product.entity.ProductEntity
+import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import reactor.core.publisher.Flux
 
 interface DBProductRepository : ReactiveCrudRepository<ProductEntity, Long> {
     fun findAllByKnitterIdAndInputStatus(knitterId: Long, inputStatus: InputStatus): Flux<ProductEntity>
+    fun findAllByKnitterIdAndIdBefore(knitterId: Long, id: Long, pageable: Pageable): Flux<ProductEntity>
+    fun findAllByKnitterId(knitterId: Long, pageable: Pageable): Flux<ProductEntity>
 }
--- src/main/kotlin/com/kroffle/knitting/infra/persistence/product/repository/R2dbcProductRepository.kt
+++ src/main/kotlin/com/kroffle/knitting/infra/persistence/product/repository/R2dbcProductRepository.kt
@@ -3,6 +3,7 @@ package com.kroffle.knitting.infra.persistence.product.repository
 import com.kroffle.knitting.domain.product.entity.Product
 import com.kroffle.knitting.domain.product.enum.InputStatus
 import com.kroffle.knitting.infra.persistence.exception.NotFoundEntity
+import com.kroffle.knitting.infra.persistence.helper.pagination.PaginationHelper
 import com.kroffle.knitting.infra.persistence.product.entity.ProductEntity
 import com.kroffle.knitting.infra.persistence.product.entity.ProductItemEntity
 import com.kroffle.knitting.infra.persistence.product.entity.ProductTagEntity
@@ -11,6 +12,7 @@ import com.kroffle.knitting.infra.persistence.product.entity.toProductItemEntiti
 import com.kroffle.knitting.infra.persistence.product.entity.toProductTagEntities
 import com.kroffle.knitting.usecase.helper.pagination.type.Paging
 import com.kroffle.knitting.usecase.helper.pagination.type.Sort
+import com.kroffle.knitting.usecase.helper.pagination.type.SortDirection
 import com.kroffle.knitting.usecase.repository.ProductRepository
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Flux
@@ -40,6 +42,42 @@ class R2dbcProductRepository(
         return Mono.zip(product, tags, items)
             .map {
                 it.t1.toProduct(it.t2, it.t3)
+            }
+    }
+
+    private fun getProductAggregate(products: Flux<ProductEntity>): Flux<Product> {
+        val productIds: Mono<List<Long>> =
+            products
+                .map { product -> product.getNotNullId() }
+                .collect(toList())
+
+        val tagMap: Mono<Map<Long, Collection<ProductTagEntity>>> =
+            productIds
+                .flatMap {
+                    // FIXME: 여러번 호출되는 이유 파악하여 고치기
+                    productTagRepository
+                        .findAllByProductIdIn(it)
+                        .collectMultimap { tag -> tag.getForeignKey() }
+                }
+        val itemMap: Mono<Map<Long, Collection<ProductItemEntity>>> =
+            productIds
+                .flatMap {
+                    productItemRepository
+                        .findAllByProductIdIn(it)
+                        .collectMultimap { item -> item.getForeignKey() }
+                }
+
+        return products
+            .concatMap {
+                product ->
+                Mono.zip(tagMap, itemMap)
+                    .map {
+                        val productId = product.getNotNullId()
+                        product.toProduct(
+                            it.t1[productId]?.map { tag -> tag.toTag() } ?: listOf(),
+                            it.t2[productId]?.map { item -> item.toItem() } ?: listOf()
+                        )
+                    }
             }
     }
 
@@ -74,46 +112,33 @@ class R2dbcProductRepository(
             .switchIfEmpty(Mono.error(NotFoundEntity(ProductEntity::class.java)))
 
     override fun getProductsByKnitterId(knitterId: Long, paging: Paging, sort: Sort): Flux<Product> {
-        TODO("Not yet implemented")
+        val pageRequest = PaginationHelper.makePageRequest(paging, sort)
+
+        val products: Flux<ProductEntity> = when (sort.direction) {
+            SortDirection.DESC ->
+                if (paging.after != null) {
+                    productRepository
+                        .findAllByKnitterIdAndIdBefore(
+                            knitterId = knitterId,
+                            id = paging.after.toLong(),
+                            pageable = pageRequest,
+                        )
+                } else {
+                    productRepository
+                        .findAllByKnitterId(
+                            knitterId = knitterId,
+                            pageable = pageRequest,
+                        )
+                }
+            else -> throw NotImplementedError()
+        }
+        return getProductAggregate(products)
     }
 
     override fun findRegisteredProduct(knitterId: Long): Flux<Product> {
         val products: Flux<ProductEntity> =
             productRepository
                 .findAllByKnitterIdAndInputStatus(knitterId, InputStatus.REGISTERED)
-
-        val productIds: Mono<List<Long>> =
-            products
-                .map {
-                    it.getNotNullId()
-                }
-                .collect(toList())
-
-        val tagMap: Mono<Map<Long, Collection<ProductTagEntity>>> =
-            productIds
-                .flatMap {
-                    productTagRepository
-                        .findAllByProductIdIn(it)
-                        .collectMultimap { tag -> tag.getForeignKey() }
-                }
-        val itemMap: Mono<Map<Long, Collection<ProductItemEntity>>> =
-            productIds
-                .flatMap {
-                    productItemRepository
-                        .findAllByProductIdIn(it)
-                        .collectMultimap { item -> item.getForeignKey() }
-                }
-        return products
-            .flatMap {
-                product ->
-                Mono.zip(tagMap, itemMap)
-                    .map {
-                        val productId = product.getNotNullId()
-                        product.toProduct(
-                            it.t1[productId]?.map { tag -> tag.toTag() } ?: listOf(),
-                            it.t2[productId]?.map { item -> item.toItem() } ?: listOf()
-                        )
-                    }
-            }
+        return getProductAggregate(products)
     }
 }
