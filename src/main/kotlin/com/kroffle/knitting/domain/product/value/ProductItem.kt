83f18f38ddab754e3d40fb39a3ecad155a1b19a3
yool <devuri404@gmail.com>
2021-09-12 17:06:09 +0900

    feat: remove id member in value classes

--- src/main/kotlin/com/kroffle/knitting/domain/product/value/ProductItem.kt
+++ src/main/kotlin/com/kroffle/knitting/domain/product/value/ProductItem.kt
@@ -3,19 +3,17 @@ package com.kroffle.knitting.domain.product.value
 import com.kroffle.knitting.domain.product.enum.ProductItemType
 import java.time.LocalDateTime
 
-/* FIXME: #99 이슈 진행하며 id 제거가 가능하다면 제거, 불가능하다면 entity 로 변경합니다. */
 abstract class ProductItem(
-    val id: Long?,
     val itemId: Long,
     val createdAt: LocalDateTime?,
 ) {
     abstract val type: ProductItemType
 
     companion object {
-        fun create(id: Long?, itemId: Long, createdAt: LocalDateTime?, type: ProductItemType): ProductItem {
+        fun create(itemId: Long, createdAt: LocalDateTime?, type: ProductItemType): ProductItem {
             return when (type) {
-                ProductItemType.DESIGN -> DesignProductItem(id, itemId, createdAt)
-                ProductItemType.GOODS -> GoodsProductItem(id, itemId, createdAt)
+                ProductItemType.DESIGN -> DesignProductItem(itemId, createdAt)
+                ProductItemType.GOODS -> GoodsProductItem(itemId, createdAt)
             }
         }
     }
