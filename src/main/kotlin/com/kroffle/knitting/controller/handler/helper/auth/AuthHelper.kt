b142f91880375628650cbd859356be4642e6e754
yool <devuri404@gmail.com>
2021-09-26 10:35:37 +0900

    refactor: make AuthHelper be object

--- src/main/kotlin/com/kroffle/knitting/controller/handler/helper/auth/AuthHelper.kt
+++ src/main/kotlin/com/kroffle/knitting/controller/handler/helper/auth/AuthHelper.kt
@@ -3,14 +3,12 @@ package com.kroffle.knitting.controller.handler.helper.auth
 import com.kroffle.knitting.controller.handler.helper.auth.exception.KnitterIdRequired
 import org.springframework.web.reactive.function.server.ServerRequest
 
-class AuthHelper {
-    companion object {
-        fun getKnitterId(request: ServerRequest): Long {
-            val knitterId = request.attribute("knitterId")
-            if (knitterId.isEmpty) {
-                throw KnitterIdRequired()
-            }
-            return knitterId.get() as Long
+object AuthHelper {
+    fun getKnitterId(request: ServerRequest): Long {
+        val knitterId = request.attribute("knitterId")
+        if (knitterId.isEmpty) {
+            throw KnitterIdRequired()
         }
+        return knitterId.get() as Long
     }
 }
