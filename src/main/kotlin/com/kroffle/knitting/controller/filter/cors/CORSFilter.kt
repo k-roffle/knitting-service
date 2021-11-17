2134c50f9305d31ab62069d933b9dd15d9eb8540
yool <devuri404@gmail.com>
2021-11-17 23:57:01 +0900

    refactor: replace string to httpHeaders constant

--- src/main/kotlin/com/kroffle/knitting/controller/filter/cors/CORSFilter.kt
+++ src/main/kotlin/com/kroffle/knitting/controller/filter/cors/CORSFilter.kt
@@ -3,6 +3,7 @@ package com.kroffle.knitting.controller.filter.cors
 import com.kroffle.knitting.infra.properties.WebApplicationProperties
 import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
@@ -20,11 +21,13 @@ class CORSFilter(private val webProperties: WebApplicationProperties) : WebFilte
         if (CorsUtils.isCorsRequest(request)) {
             val response = exchange.response
             val headers = response.headers
-            headers.add("Access-Control-Allow-Origin", webProperties.origins.joinToString(","))
-            headers.add("Access-Control-Allow-Methods", "*")
-            headers.add("Access-Control-Max-Age", "3600")
-            headers.add("Access-Control-Allow-Headers", "Authorization")
-            headers.add("Access-Control-Allow-Headers", "Content-Type")
+
+            headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, webProperties.origins.joinToString(","))
+            headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "*")
+            headers.add(HttpHeaders.ACCESS_CONTROL_MAX_AGE, "3600")
+            headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "Authorization")
+            headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "Content-Type")
+
             if (request.method == HttpMethod.OPTIONS) {
                 response.statusCode = HttpStatus.OK
                 return Mono.empty()
