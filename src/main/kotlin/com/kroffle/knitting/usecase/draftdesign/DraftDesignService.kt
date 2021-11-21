87369a1257cbdccc84f44c6857d9062cd0249bb0
yool <devuri404@gmail.com>
2021-11-22 00:19:41 +0900

    feat: implement service layer about saving draft design

---
+++ src/main/kotlin/com/kroffle/knitting/usecase/draftdesign/DraftDesignService.kt
@@ -0,0 +1,55 @@
+package com.kroffle.knitting.usecase.draftdesign
+
+import com.kroffle.knitting.domain.design.entity.Design
+import com.kroffle.knitting.domain.draftdesign.entity.DraftDesign
+import com.kroffle.knitting.usecase.draftdesign.dto.SaveDraftDesignData
+import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
+
+@Service
+class DraftDesignService(
+    private val draftDesignRepository: DraftDesignRepository,
+    private val designRepository: DesignRepository,
+) {
+    private fun verifyDesignId(designId: Long, knitterId: Long): Mono<Design> {
+        return designRepository
+            .findByIdAndKnitterId(designId, knitterId)
+    }
+
+    private fun saveDraftDesign(data: SaveDraftDesignData): Mono<DraftDesign> {
+        return if (data.id == null) {
+            draftDesignRepository.save(
+                DraftDesign.new(
+                    knitterId = data.knitterId,
+                    designId = data.designId,
+                    value = data.value,
+                )
+            )
+        } else {
+            draftDesignRepository
+                .findByIdAndKnitterId(data.id, data.knitterId)
+                .map { it.merge(data.value) }
+                .flatMap {
+                    draftDesignRepository.save(it)
+                }
+        }
+    }
+
+    fun saveDraft(data: SaveDraftDesignData): Mono<DraftDesign> {
+        return if (data.designId != null) {
+            verifyDesignId(data.designId, data.knitterId)
+                .flatMap { saveDraftDesign(data) }
+        } else {
+            saveDraftDesign(data)
+        }
+    }
+
+    interface DraftDesignRepository {
+        fun findByIdAndKnitterId(id: Long, knitterId: Long): Mono<DraftDesign>
+        fun save(draftDesign: DraftDesign): Mono<DraftDesign>
+    }
+
+    interface DesignRepository {
+        fun findByIdAndKnitterId(id: Long, knitterId: Long): Mono<Design>
+    }
+}
