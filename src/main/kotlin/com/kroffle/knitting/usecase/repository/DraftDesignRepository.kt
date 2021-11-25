55c09f09dd7b161a617d3396b9e621824fb80680
yool <devuri404@gmail.com>
2021-11-25 23:34:45 +0900

    feat: implement necessary draft design repository method

--- src/main/kotlin/com/kroffle/knitting/usecase/repository/DraftDesignRepository.kt
+++ src/main/kotlin/com/kroffle/knitting/usecase/repository/DraftDesignRepository.kt
@@ -1,5 +1,6 @@
 package com.kroffle.knitting.usecase.repository
 
+import com.kroffle.knitting.usecase.design.DesignService
 import com.kroffle.knitting.usecase.draftdesign.DraftDesignService
 
-interface DraftDesignRepository : DraftDesignService.DraftDesignRepository
+interface DraftDesignRepository : DraftDesignService.DraftDesignRepository, DesignService.DraftDesignRepository
