1ed4ee83cb29b1644f261579c5d848202c31cf8d
yool <devuri404@gmail.com>
2021-11-26 16:26:52 +0900

    feat: add https authorization api

---
+++ src/main/kotlin/com/kroffle/knitting/controller/router/auth/CertRouter.kt
@@ -0,0 +1,29 @@
+package com.kroffle.knitting.controller.router.auth
+
+import com.kroffle.knitting.controller.handler.auth.CertHandler
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.server.RequestPredicates
+import org.springframework.web.reactive.function.server.RouterFunctions
+import org.springframework.web.reactive.function.server.router
+
+@Configuration
+class CertRouter(private val handler: CertHandler) {
+    @Bean
+    fun certRouterFunction() = RouterFunctions.nest(
+        RequestPredicates.path(ROOT_PATH),
+        router {
+            listOf(
+                GET(APPLY_HTTPS_PATH, handler::applyHttps),
+            )
+        }
+    )
+
+    companion object {
+        private const val ROOT_PATH = "/"
+        private const val APPLY_HTTPS_PATH = ".well-known/acme-challenge/o7DtLtR3ccPKK71iDectTrZC6tftXInhTFDAuDNgYI0"
+        val PUBLIC_PATHS = listOf(
+            "$ROOT_PATH$APPLY_HTTPS_PATH",
+        )
+    }
+}
