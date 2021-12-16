b7b9a96b0c630866161c7e23ef2dbced4e62b7fc
yool <devuri404@gmail.com>
2021-12-17 00:11:22 +0900

    feat: make DraftDesign parse value to get name

--- src/main/kotlin/com/kroffle/knitting/domain/draftdesign/entity/DraftDesign.kt
+++ src/main/kotlin/com/kroffle/knitting/domain/draftdesign/entity/DraftDesign.kt
@@ -1,5 +1,9 @@
 package com.kroffle.knitting.domain.draftdesign.entity
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter
+import com.fasterxml.jackson.annotation.JsonAnySetter
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import java.time.OffsetDateTime
 
 data class DraftDesign(
@@ -16,7 +20,12 @@ data class DraftDesign(
             updatedAt = OffsetDateTime.now(),
         )
 
+    val name: String?
+        get() = OBJECT_MAPPER.readValue<Value>(this.value).name
+
     companion object {
+        private val OBJECT_MAPPER = ObjectMapper()
+
         fun new(knitterId: Long, designId: Long?, value: String): DraftDesign =
             DraftDesign(
                 id = null,
@@ -27,4 +36,11 @@ data class DraftDesign(
                 updatedAt = OffsetDateTime.now(),
             )
     }
+
+    data class Value(
+        val name: String? = null,
+        @JsonAnySetter
+        @get:JsonAnyGetter
+        val truncated: MutableMap<String, Any> = sortedMapOf(),
+    )
 }
