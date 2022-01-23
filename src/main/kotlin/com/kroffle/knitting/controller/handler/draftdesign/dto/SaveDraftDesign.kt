5dded4bbee376855a6f8e7ed3910b734a293dc66
yool <devuri404@gmail.com>
2022-01-23 19:08:08 +0900

    fix: add JsonProperty annotation for snake_case

--- src/main/kotlin/com/kroffle/knitting/controller/handler/draftdesign/dto/SaveDraftDesign.kt
+++ src/main/kotlin/com/kroffle/knitting/controller/handler/draftdesign/dto/SaveDraftDesign.kt
@@ -1,10 +1,12 @@
 package com.kroffle.knitting.controller.handler.draftdesign.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.kroffle.knitting.controller.handler.helper.response.type.ObjectPayload
 
 object SaveDraftDesign {
     data class Request(
         val id: Long?,
+        @JsonProperty("design_id")
         val designId: Long?,
         val value: String,
     )
