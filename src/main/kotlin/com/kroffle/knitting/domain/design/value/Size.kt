8182705fe8d3f33c9d57a898b24b28220455d13b
yool <devuri404@gmail.com>
2022-01-23 19:54:26 +0900

    feat: add JsonNaming annotation for snake_case strategy

--- src/main/kotlin/com/kroffle/knitting/domain/design/value/Size.kt
+++ src/main/kotlin/com/kroffle/knitting/domain/design/value/Size.kt
@@ -1,5 +1,9 @@
 package com.kroffle.knitting.domain.design.value
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class Size(
     val totalLength: Length,
     val sleeveLength: Length,
