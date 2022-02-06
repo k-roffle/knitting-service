d6c30067faf2e6327a3cb35b5930920f30f6547f
yool <devuri404@gmail.com>
2022-02-06 23:34:53 +0900

    test: remove unused extension

--- src/test/kotlin/com/kroffle/knitting/helper/extension/Money.kt
+++
@@ -1,8 +0,0 @@
-package com.kroffle.knitting.helper.extension
-
-import com.kroffle.knitting.domain.value.Money
-
-fun Money.like(other: Money): Boolean {
-    if (this === other) return true
-    return value == other.value
-}
